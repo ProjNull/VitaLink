ecd139d2368036338b54f52464e2d25ddd11f541
SomeKristi <kmaas2007@gmail.com>
2023-12-08 21:11:16 +0100

    added JWT example

---
+++ backend/tokens.py
@@ -0,0 +1,10 @@
+import jwt
+
+payload = {"user_id": 123, "username": "john_doe"}
+secret_key = "bimbim"
+
+encoded_token = jwt.encode(payload, secret_key, algorithm="HS256")
+print("Encoded JWT:", encoded_token)
+
+decoded_payload = jwt.decode(encoded_token, secret_key, algorithms=["HS256"])
+print("Decoded Payload:", decoded_payload)
