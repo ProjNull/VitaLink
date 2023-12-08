a1e8db43fb409058bdd080a47c0e73e4d32ffced
Kaiy <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-12-08 13:47:58 +0100

    Adding basics

---
+++ backend/docs.md
@@ -0,0 +1,15 @@
+# API Documentation
+---
+## Basics
+---
+- Json responses
+
+### Routes
+---
+### Test
+### POST/GET /test
+
+- **Params**
+    - None
+- **Response** 
+    - Message: {Method} request
--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,6 @@ from database import Session, func
 from flask import Flask, json, jsonify, request, session
 from flask_socketio import SocketIO
 from exampleModel import Users
-from werkzeug.exceptions import HTTPException
-from werkzeug.security import check_password_hash, generate_password_hash
 
 SECRET_KEY = "CHANGE-ME-TO-SOMETHING-RANDOM"
 
@@ -18,6 +16,19 @@ api.secret_key = SECRET_KEY
 
 socketio = SocketIO(api)
 socketio.init_app(api, cors_allowed_origins="*")
+
+@api.route("/test", methods=["GET","POST"])
+def test():
+    """
+        Handle testing if API works
+        
+        :return: message: {method} request
+    """
+    if request.method == "POST":
+        return jsonify({"message": "POST request"})
+    if request.method == "GET":
+        return jsonify({"message": "GET request"})    
+
 
 if __name__ == "__main__":
     socketio.run(api, debug=True, host="0.0.0.0", port="8002")
