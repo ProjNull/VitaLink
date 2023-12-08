053f302d67ece9267831c5ca54bf97ec7745e006
SomeKristi <kmaas2007@gmail.com>
2023-12-09 00:46:17 +0100

    Added qrcode route

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from Database.database import Base, engine
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_socketio import SocketIO
 
 Base.metadata.create_all(engine)
@@ -28,6 +28,36 @@ def root():
     """
     return {"message": request.method + " request", "status": 200}
 
+from io import BytesIO
+import qrcode
+
+@app.route("/genqr")
+def generate_qr_code():
+    # Data to encode in the QR code
+    data_to_encode = request.args.get(
+        "data", "Default Data - No data received")
+
+    # Generate QR code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data_to_encode)
+    qr.make(fit=True)
+
+    # Create an image from the QR code
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img_buffer = BytesIO()
+
+    # Save the image to the buffer
+    img.save(img_buffer)
+    img_buffer.seek(0)
+
+    # Serve the image from the buffer
+    return send_file(img_buffer, mimetype="image/png")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, host="0.0.0.0", port="8002")
