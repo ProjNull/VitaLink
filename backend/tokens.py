4c9d6d390220080884e0fec2475c64fc5f34f9cb
SomeKristi <kmaas2007@gmail.com>
2023-12-08 21:22:55 +0100

    minor value change

--- backend/tokens.py
+++ backend/tokens.py
@@ -6,5 +6,5 @@ secret_key = "bimbim"
 encoded_token = jwt.encode(payload, secret_key, algorithm="HS256")
 print("Encoded JWT:", encoded_token)
 
-decoded_payload = jwt.decode(encoded_token, secret_key, algorithms=["HS256"])
+decoded_payload = jwt.decode(encoded_token, secret_key, algorithms="HS256")
 print("Decoded Payload:", decoded_payload)
