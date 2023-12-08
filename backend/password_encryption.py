ec7a319996925b857a2e3f0e82bbc3494619a5dc
SomeKristi <kmaas2007@gmail.com>
2023-12-08 19:32:26 +0100

    fixed encryption requireing bytes not string

--- backend/password_encryption.py
+++ backend/password_encryption.py
@@ -4,4 +4,4 @@ def hashPassword(password:str)->str:
     toBeHashed = password.encode("utf-8")
     return bcrypt.hashpw(toBeHashed, bcrypt.gensalt())
 def checkPassword(password:str, hashed:str)->bool:
-    return bcrypt.checkpw(password, hashed)
+    return bcrypt.checkpw(password.encode("utf-8"), hashed.encode("utf-8"))
