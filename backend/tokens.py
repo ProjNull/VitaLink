4f20620a15c485ea6075cf0ff85d8e7dff68506f
SomeKristi <kmaas2007@gmail.com>
2023-12-08 22:02:01 +0100

    finished the wraper for JWT

--- backend/tokens.py
+++ backend/tokens.py
@@ -1,31 +1,28 @@
+from ast import Dict
+from pickle import NONE
 import jwt
 import decouple
 import secrets
 
 secret_key = decouple.config("jwt_secret", secrets.token_bytes(64).decode())
 
-payload = {"user_id": 123, "username": "john_doe"}
 
-encoded_token = jwt.encode(payload, secret_key, algorithm="HS256")
-print("Encoded JWT:", encoded_token)
+# encoded_token = jwt.encode(payload, secret_key, algorithm="HS256")
+# print("Encoded JWT:", encoded_token)
 
-decoded_payload = jwt.decode(encoded_token, secret_key, algorithms="HS256")
-print("Decoded Payload:", decoded_payload)
+# decoded_payload = jwt.decode(encoded_token, secret_key, algorithms="HS256")
+# print("Decoded Payload:", decoded_payload)
 
-def generateJWTPacient():
-    firstName: str
+def generateJWTPacient(firstName: str, lastName: str, idPacient: int):
+    payload = {"firstName": firstName, "lastName": lastName, "idPacient" :idPacient}##cratzy commen
+    return jwt.encode(payload, secret_key, algorithm="HS256")##cratzy commen
+##cratzy commen
+def generateJWTEmployee(firstName: str, lastName: str, idEmployee: int, isAdmin: bool):##cratzy commen
+    payload = {"firstName": firstName, "lastName": lastName, "idEmployee" :idEmployee}
+    return jwt.encode(payload, secret_key, algorithm="HS256")##cratzy commen
 
-    lastName: str
-
-    pacientId: int
-def generateJWTEmployee():
-
-    firstName: str
-
-    lastName: str
-
-    employeeId: int
-
-    isAdmin: bool
-def validateJWT(jwtData) -> dict | None:
-    pass
+def validateJWT(token: str) -> Dict | None:##cratzy commen
+    try:##cratzy commen
+        return jwt.decode(token, secret_key, algorithms=["HS256"])##cratzy commen
+    except:##cratzy commen
+        return None
